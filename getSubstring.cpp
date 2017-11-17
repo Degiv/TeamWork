ca00be5fa6b5db14c40e1deac58cec0c50568445
Degiv <32798005+Degiv@users.noreply.github.com>
2017-11-17 22:26:01 +0300

    Update getSubstring.cpp

--- getSubstring.cpp
+++ getSubstring.cpp
@@ -2,13 +2,16 @@
 
 // [first, last)
 String getSubstring(String &string, int first, int last) {
+	String result;
+	if (last > string.size || first >= last)
+		return result;
+	
 	int newSize = last - first;
 	char* subData = new char[newSize];
 
 	for (int i = first; i < last; ++i) 
 		subData[i - first] = string.data[i];
 
-	String result;
 	result.data = subData;
 	result.size = newSize;
 
