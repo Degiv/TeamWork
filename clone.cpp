58183138959344414916355107ebec25aebefefb
Andrey Khodko <khodand@outlook.com>
2017-11-17 16:33:54 +0300

    Update clone.cpp

--- clone.cpp
+++ clone.cpp
@@ -1,14 +1,14 @@
 #include "string.h>
 
-String* clone(String &string)
+String clone(String &string)
 {
-    String* newString = new String;
-    newString->size = string.size;
-    newString->data = new char[newString->size];
+    String newString;
+    newString.size = string.size;
+    newString.data = new char[newString.size];
 
-    for (int i = 0; i < newString->size; i++)
+    for (int i = 0; i < newString.size; i++)
     {
-        newString->data[i] = string.data[i];
+        newString.data[i] = string.data[i];
     }
 
     return newString;
